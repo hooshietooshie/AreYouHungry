37129f0c50511e390c178b9be88955144dde7dbd
HarshangShah <shah0300@algonquinlive.com>
2022-04-05 17:05:30 -0400

    Initial commit

---
+++ app/src/main/java/com/cst2335/areyouhungry/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.cst2335.areyouhungry.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
